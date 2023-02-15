f00c91a9abb8a88d94bafd8580242040fa38575f
hak.dok0@gmail.com <hak.dok0@gmail.com>
2023-02-15 19:26:28 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DitributiveSystem.Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DitributiveSystem.Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string firstName, string lastName, string email) {
+            
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ErrorMessage = "One of the fields is empty.";
+                return View();
+            }
+            ViewBag.ErrorMessage = "";
+            return Content($"FirstName: {firstName}\nLastName: {lastName}\nEmail: {email}");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DitributiveSystem.Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
